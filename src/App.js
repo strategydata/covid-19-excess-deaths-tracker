eb0dd0b251999f3a412268a992411a054ddf316a
Kang Wang <kangqiwang@outlook.com>
2022-11-21 16:36:08 +0000

    update main app layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Alert from "./components/layout/Alert"
 
 function App() {
 
   return (
     <Router>
       <div className="App">
-        <AppHeader className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </AppHeader>
-        <InfoDetail></InfoDetail>
-        <MapView></MapView>
+        <Navbar />
+        <div className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/user/:login" component={User} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
