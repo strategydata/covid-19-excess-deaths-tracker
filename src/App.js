11eb463c27107fde0b1e31761be9dd430615d299
Kang Wang <kangqiwang@outlook.com>
2022-11-25 15:07:50 +0000

    update css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import './App.css';
-import {  Routes, Route, Router } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
@@ -14,8 +14,8 @@ function App() {
         <div className='container'>
           {/* <Alert /> */}
           <Routes>
-            <Route path="/" component={Home} />
-            <Route path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
             {/* <Route exact path="/user/:login" component={User} /> */}
             <Route path="/404" component={NotFound} />
           </Routes>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,35 @@
 import React from "react";
-import {GiCat} from "react-icons/gi";
+import { GiCat } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const Navbar = ({ icon, title }) => {
+const Navbar = ({ title }) => {
 
     return (
         <nav className="navbar bg-primary">
             <h1>
-                <GiCat /> Amber Data
+                <GiCat /> {title}
             </h1>
             <ul>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
                 <li>
+                    <Link to="/map">Map</Link>
+                </li>
+                <li>
                     <Link to='/about'>About</Link>
                 </li>
             </ul>        </nav>
     );
+};
+
+Navbar.defaultProps = {
+    title: "Amber Data"
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired
 };
 
 export default Navbar;
